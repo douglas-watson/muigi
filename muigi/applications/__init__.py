e59de6a24f24d8b5c1961cee095c8b8aece3ee68
Douglas Watson <douglas@watsons.ch>
2011-08-30 08:32:02 +0200

    Added easydaq_settings.py and created applications subpackage.

---
+++ muigi/applications/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: UTF8 -*-
+#
+#   muigi.applications - where customized code for inididual chips is kept.
+#
+#   AUTHOR: Douglas Watson <douglas@watsons.ch>
+#
+#   DATE: started on 30 August 2011
+#
+#   LICENSE: GNU GPL
+#
+#################################################
+
+"""
+The Muigi applications sub-package
+==================================
+
+Control microfluidic chips from an application point of view: instead of
+controlling valves, call functions (such as flush(), pump(freq), etc.).
+
+"""
